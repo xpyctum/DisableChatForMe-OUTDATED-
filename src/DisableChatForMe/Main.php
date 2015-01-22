4380adda897e106ad2a5835a9fa77b72ea6bf125
xpyctum <basalygaoleg@yandex.ru>
2015-01-22 16:01:31 +0500

    Update plugin. Fixed small bug

--- src/DisableChatForMe/Main.php
+++ src/DisableChatForMe/Main.php
@@ -11,11 +11,21 @@ use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 
 class DisableChat extends PluginBase implements Listener{
+    /**
+     * @var array
+     */
     private $players = [];
     public function onEnable(){}
 
     public function onDisable(){}
 
+    /**
+     * @param CommandSender $sender
+     * @param Command $command
+     * @param string $label
+     * @param array $args
+     * @return bool|void
+     */
     public function onCommand(CommandSender $sender, Command $command,$label,array $args){
         switch($command->getName()){
             case "chaton":{
@@ -30,16 +40,32 @@ class DisableChat extends PluginBase implements Listener{
             }
         }
     }
+
+    /**
+     * @param Player $player
+     * @return bool
+     */
     public function ischatoff(Player $player){
         return in_array($player->getName(), $this->players);
     }
+
+    /**
+     * @param Player $player
+     */
     public function chaton(Player $player){
         unset($this->players[$player->getName()]);
     }
+
+    /**
+     * @param Player $player
+     */
     public function chatoff(Player $player){
         $this->players[] = $player->getName();
     }
 
+    /**
+     * @param PlayerChatEvent $event
+     */
     public function onChat(PlayerChatEvent $event){
         if($event->getPlayer() instanceof Player){
             foreach($this->getServer()->getOnlinePlayers() as $player){
@@ -50,9 +76,9 @@ class DisableChat extends PluginBase implements Listener{
                     $user = $event->getPlayer()->getDisplayName();
                     $player->sendMessage("<".$user."> ".$message);
                 }
-                $event->setCancelled(true);
-            }
 
+            }
+            $event->setCancelled(true);
         }
     }
 }
